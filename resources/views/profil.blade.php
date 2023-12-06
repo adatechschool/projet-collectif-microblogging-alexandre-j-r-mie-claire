0236929e3fa6bab35ea25efab8a4d35acb6f4280
Lachignol <110435478+Lachignol@users.noreply.github.com>
2023-12-06 19:34:19 +0100

    profilpage

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -25,14 +25,27 @@
         </div>
 
 
-<div class="max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md">
+<div class="max-w-2xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md mt-4">
     
 <div class="mt-2"><a href="#" class="text-2xl font-bold text-gray-700 hover:underline">Biography.</a>
-    <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+    <p class="mt-2  text-l text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
         Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
-        reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
+        reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque architecto sunt distinctio ipsum laborum. Nemo itaque optio quibusdam voluptatum architecto quae cupiditate omnis perspiciatis, sequi ipsum dolorem expedita mollitia deserunt. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe officiis atque eligendi? Consequatur ad adipisci esse eius itaque. Qui sunt dolor nemo, saepe esse fuga voluptatem mollitia consequatur voluptatum omnis.</p>
 </div>
 </div>
+
+<div class="mt-6">
+    <div class="max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md">
+        <div class="flex items-center justify-between"><span class="font-light text-gray-600">Jun 1,
+                2020</span><a href="#"
+                class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">Laravel</a>
+        </div>
+        <div class="mt-2"><a href="#" class="text-2xl font-bold text-gray-700 hover:underline">Build
+                Your New Idea with Laravel Freamwork.</a>
+            <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
+                reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
+        </div>
 
 
 </div>

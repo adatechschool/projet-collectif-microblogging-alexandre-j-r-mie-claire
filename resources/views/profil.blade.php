a50feba84c2ee03b0818353d1a9954e68ced29e4
Lachignol <110435478+Lachignol@users.noreply.github.com>
2023-12-07 17:16:37 +0100

    categorypage

--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -2,33 +2,33 @@
 
 
 <div class="flex-col flex">
-        <div class="flex-row">
-       <div class=" flex justify-center ">
     
-        <a href="#" class="mt-4 ml-2 ">
-            <img
-                src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;w=1780&amp;q=80"
-                alt="avatar" class="hidden object-cover w-20 h-20 mx-4 rounded-full sm:block">
-            <h1 class="font-bold text-2xl text-gray-700 hover:underline">Alex John</h1>
-        </a>
-    </div>
-    <div class="flex justify-end">
-    <div class="flex flex-col max-w-sm px-6 py-4  bg-white rounded-lg shadow-md">
-<ul class="-mx-4">
-    <li class="flex items-center">Age
+    
+    
+    
+    
+    <div class="max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md mt-4 ">
+            <div class="flex-row">
+           <div class=" flex justify-center ">
         
-        <li><a href="#" class="mx-1 font-bold text-gray-700 hover:underline">Alex John</li><span
-                class="text-sm font-light text-gray-700">Created 23 Posts</span></p>
-    </li>
-</div>
-    </div>
-    </div>
-       
-       
-       
-
-
-        <div class="max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md ">
+            <a href="#" class=" mt-4 ml-2 ">
+                <img
+                    src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;w=1780&amp;q=80"
+                    alt="avatar" class="hidden object-cover w-20 h-20 mx-4 rounded-full sm:block">
+                <h1 class="font-bold text-2xl text-gray-700 hover:underline">Alex John</h1>
+            </a>
+        </div>
+        <div class="flex justify-end">
+        <div class="flex flex-col max-w-sm px-6 py-4  bg-white rounded-lg shadow-md">
+        <ul class="-mx-4">
+        <li class="flex items-center">Age
+            
+            <li><a href="#" class="mx-1 font-bold text-gray-700 hover:underline">Alex John</li><span
+                    class="text-sm font-light text-gray-700">Created 23 Posts</span></p>
+        </li>
+        </div>
+        </div>
+        </div>
     
 <div class="mt-2"><a href="#" class="text-2xl font-bold text-gray-700 hover:underline">Biography.</a>
     <p class="mt-2  text-l text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.

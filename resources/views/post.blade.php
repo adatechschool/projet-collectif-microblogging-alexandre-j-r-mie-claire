c01af7fbea60294767f887a2f5e52211ff612323
Lachignol <110435478+Lachignol@users.noreply.github.com>
2023-12-07 12:37:49 +0100

    postpage

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,54 +7,43 @@
             
 <div class="flex-col ">
           
-  <div class="flex-row flex justify-end">
-    <div class="hidden w-4/12 -mx-8 lg:block ">
-            <div class="px-8 flex-row justify-end">
-                <h1 class="mb-4 text-xl font-bold text-gray-700">Authors</h1>
-                <div class="flex flex-col max-w-sm px-6 py-4 mx-auto bg-white rounded-lg shadow-md">
-                    <ul class="-mx-4">
-                                    <li class="flex items-center mt-6"><img
-                                        src="https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=333&amp;q=80"
-                                        alt="avatar" class="object-cover w-10 h-10 mx-4 rounded-full">
-                                        <p><a href="#" class="mx-1 font-bold text-gray-700 hover:underline">Jane Doe</a><span
-                                            class="text-sm font-light text-gray-700">Created 52 Posts</span></p>
-                                        </li>
-                                        </p>
-                                    </li>
-                                </ul>
-                </div>
-            </div>
-        </div>
-    </div>
+    
 
-
-    <div class="flex items-center px-6 py-8">
-        <div class="container flex justify-between mx-auto">
-            <div class="w-full lg:w-8/12">
-                <div class="flex items-center justify-between">
-                    <h1 class="text-xl font-bold text-gray-700 md:text-2xl">Post</h1>
-                    
-                </div>
                 <div class="mt-6 flex">
-                    <div class="max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md">
+                    <div class=" mb-4 max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md">
                         <img
-                                      src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80"
-                                      alt="avatar" class="hidden object-cover w-32 h-32 mx-4  sm:block">
+                        src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80"
+                        alt="avatar" class="hidden object-cover w-32 h-32 mx-4  sm:block">
                         <div class="flex items-center justify-between"><span class="font-light text-gray-600">Jun 1,
-                                2020</span><a href="#"
-                                class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">Laravel</a>
+                            2020</span><a href="#"
+                            class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">Laravel</a>
                         </div>
                         <div class="mt-2"><a href="#" class="text-2xl font-bold text-gray-700 hover:underline">Build
                                 Your New Idea with Laravel Freamwork.</a>
-                            <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                                Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
-                                reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
-                        </div>
-                        <div class="flex items-center justify-between mt-4"><a href="#"
-                                class="text-blue-500 hover:underline">Read more</a>
-                                        <a href="#" class="flex items-center"><h1 class="font-bold text-gray-700 hover:underline">Alex John</h1></a>
-                                
-                        </div>
+                                <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                                    Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
+                                    reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero! Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quos sunt sed vitae minima! Voluptatum earum quos veritatis inventore. Eligendi recusandae incidunt saepe alias quis accusamus reiciendis, voluptatem maiores pariatur fuga.lorem Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolores, quam? Et fugiat dolorem dolores impedit asperiores ipsum qui, quis sint expedita molestias explicabo, dicta perferendis, dolor nam facilis voluptates quisquam?</p>
+                                </div>
+                               
+                                <div class="flex-row flex justify-end">
+                                  <div class="hidden w-4/12 -mx-8 lg:block ">
+                                          <div class="px-8 flex-row justify-end">
+                                              <h1 class=" text-xl flex justify-center font-bold text-gray-700">Author</h1>
+                                              <div class="flex flex-col max-w-sm px-6 py-4 mx-auto bg-white rounded-lg shadow-md">
+                                                  <ul class="-mx-4">
+                                                                  <li class="flex justify-center "><img
+                                                                      src="https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=333&amp;q=80"
+                                                                      alt="avatar" class="object-cover w-10 h-10 mx-4 rounded-full">
+                                                                      <p><a href="#" class="mx-1 font-bold text-gray-700 hover:underline">Jane Doe</a><span
+                                                                          class="text-sm font-light text-gray-700">Created 52 Posts</span></p>
+                                                                      </li>
+                                                                      </p>
+                                                                  </li>
+                                                              </ul>
+                                              </div>
+                                          </div>
+                                      </div>
+                                  </div>
                     </div>
                 
                     </div>
--- resources/views/profil.blade.php
+++ resources/views/profil.blade.php
@@ -2,30 +2,33 @@
 
 
 <div class="flex-col flex">
+        <div class="flex-row">
+       <div class=" flex justify-center ">
     
-       <div class="flex-col flex justify-start">
-    
-        <a href="#" class=" mt-10 ml-2">
+        <a href="#" class="mt-4 ml-2 ">
             <img
                 src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;w=1780&amp;q=80"
                 alt="avatar" class="hidden object-cover w-20 h-20 mx-4 rounded-full sm:block">
             <h1 class="font-bold text-2xl text-gray-700 hover:underline">Alex John</h1>
         </a>
-        </div>
-       
-       
-        <div class="flex justify-start">
-        <div class="flex flex-col max-w-sm px-6 py-4  bg-white rounded-lg shadow-md">
-    <ul class="-mx-4">
-        <li class="flex items-center">Age
-            <p><a href="#" class="mx-1 font-bold text-gray-700 hover:underline">Alex John</a><span
-                    class="text-sm font-light text-gray-700">Created 23 Posts</span></p>
-        </li>
     </div>
-        </div>
+    <div class="flex justify-end">
+    <div class="flex flex-col max-w-sm px-6 py-4  bg-white rounded-lg shadow-md">
+<ul class="-mx-4">
+    <li class="flex items-center">Age
+        
+        <li><a href="#" class="mx-1 font-bold text-gray-700 hover:underline">Alex John</li><span
+                class="text-sm font-light text-gray-700">Created 23 Posts</span></p>
+    </li>
+</div>
+    </div>
+    </div>
+       
+       
+       
 
 
-<div class="max-w-2xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md mt-4">
+        <div class="max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md ">
     
 <div class="mt-2"><a href="#" class="text-2xl font-bold text-gray-700 hover:underline">Biography.</a>
     <p class="mt-2  text-l text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
@@ -33,9 +36,14 @@
         reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero! Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eaque architecto sunt distinctio ipsum laborum. Nemo itaque optio quibusdam voluptatum architecto quae cupiditate omnis perspiciatis, sequi ipsum dolorem expedita mollitia deserunt. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Saepe officiis atque eligendi? Consequatur ad adipisci esse eius itaque. Qui sunt dolor nemo, saepe esse fuga voluptatem mollitia consequatur voluptatum omnis.</p>
 </div>
 </div>
+</div>
 
 <div class="mt-6">
-    <div class="max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md">
+    <div class="flex items-center justify-between">
+        <h1 class="text-xl font-bold mx-auto text-gray-700 md:text-2xl">Post</h1>
+
+    </div>
+    <div class="max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md mt-4">
         <div class="flex items-center justify-between"><span class="font-light text-gray-600">Jun 1,
                 2020</span><a href="#"
                 class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">Laravel</a>

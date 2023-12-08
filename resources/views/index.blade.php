0a7327338d8259f81f1d1c9456d99816dbf86ecc
Lachignol <110435478+Lachignol@users.noreply.github.com>
2023-12-08 22:58:26 +0100

    ajoutlike et comment  post

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -46,8 +46,7 @@
                                     class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">Design</a>
                             </div>
                             <div class="mt-2"><a href="#"
-                                    class="text-2xl font-bold text-gray-700 hover:underline">Accessibility tools for
-                                    designers and developers</a>
+                                    class="text-2xl font-bold text-gray-700 hover:underline"><img src="https://placekitten.com/400/300" alt="Post Image" class="w-full h-48 object-cover rounded-md"></a>
                                 <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                                     Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
                                     reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
@@ -84,67 +83,127 @@
                     </div>
                     <div class="mt-6">
                         <div class="max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md">
-                            <div class="flex items-center justify-between"><span class="font-light text-gray-600">Feb 14,
+                            <div class="flex items-center justify-between"><span class="font-light text-gray-600">mar 4,
                                     2019</span><a href="#"
-                                    class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">PHP</a>
+                                    class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">Design</a>
                             </div>
-                            <div class="mt-2"><a href="#" class="text-2xl font-bold text-gray-700 hover:underline">PHP:
-                                    Array to Map</a>
+                            <div class="mt-2"><a href="#"
+                                    class="text-2xl font-bold text-gray-700 hover:underline"><img src="https://placekitten.com/400/300" alt="Post Image" class="w-full h-48 object-cover rounded-md"></a>
                                 <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                                     Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
                                     reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
                             </div>
-                            <div class="flex items-center justify-between mt-4"><a href="#"
-                                    class="text-blue-500 hover:underline">Read more</a>
+                            <div class="flex items-center justify-between mt-4">
                                 <div><a href="#" class="flex items-center"><img
-                                            src="https://images.unsplash.com/photo-1531251445707-1f000e1e87d0?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=281&amp;q=80"
+                                            src="https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=333&amp;q=80"
                                             alt="avatar" class="hidden object-cover w-10 h-10 mx-4 rounded-full sm:block">
-                                        <h1 class="font-bold text-gray-700 hover:underline">Lisa Way</h1>
+                                        <h1 class="font-bold text-gray-700 hover:underline">Jane Doe</h1>
                                     </a></div>
+                                     <!-- Like and Comment Section -->
+                                     <div class="flex items-center justify-between text-gray-500">
+                                        <div class="flex items-center space-x-2">
+                                            <button class="flex justify-center items-center gap-2 px-2 hover:bg-gray-50 rounded-full p-1">
+                                                <svg class="w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                                    <path d="M12 21.35l-1.45-1.32C6.11 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-4.11 6.86-8.55 11.54L12 21.35z" />
+                                                </svg>
+                                                <span>42 Likes</span>
+                                            </button>
+                                        </div>
+                                        <button class="flex justify-center items-center gap-2 px-2 hover:bg-gray-50 rounded-full p-1">
+                                            <svg width="22px" height="22px" viewBox="0 0 24 24" class="w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg">
+                                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                                <g id="SVGRepo_iconCarrier">
+                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22ZM8 13.25C7.58579 13.25 7.25 13.5858 7.25 14C7.25 14.4142 7.58579 14.75 8 14.75H13.5C13.9142 14.75 14.25 14.4142 14.25 14C14.25 13.5858 13.9142 13.25 13.5 13.25H8ZM7.25 10.5C7.25 10.0858 7.58579 9.75 8 9.75H16C16.4142 9.75 16.75 10.0858 16.75 10.5C16.75 10.9142 16.4142 11.25 16 11.25H8C7.58579 11.25 7.25 10.9142 7.25 10.5Z"></path>
+                                                </g>
+                                            </svg>
+                                            <span>3 Comment</span>
+                                        </button>
+                                    </div>
                             </div>
                         </div>
                     </div>
                     <div class="mt-6">
                         <div class="max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md">
-                            <div class="flex items-center justify-between"><span class="font-light text-gray-600">Dec 23,
-                                    2018</span><a href="#"
-                                    class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">Django</a>
+                            <div class="flex items-center justify-between"><span class="font-light text-gray-600">mar 4,
+                                    2019</span><a href="#"
+                                    class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">Design</a>
                             </div>
-                            <div class="mt-2"><a href="#" class="text-2xl font-bold text-gray-700 hover:underline">Django
-                                    Dashboard - Learn by Coding</a>
+                            <div class="mt-2"><a href="#"
+                                    class="text-2xl font-bold text-gray-700 hover:underline"><img src="https://placekitten.com/400/300" alt="Post Image" class="w-full h-48 object-cover rounded-md"></a>
                                 <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                                     Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
                                     reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
                             </div>
-                            <div class="flex items-center justify-between mt-4"><a href="#"
-                                    class="text-blue-500 hover:underline">Read more</a>
+                            <div class="flex items-center justify-between mt-4">
                                 <div><a href="#" class="flex items-center"><img
-                                            src="https://images.unsplash.com/photo-1500757810556-5d600d9b737d?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=735&amp;q=80"
+                                            src="https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=333&amp;q=80"
                                             alt="avatar" class="hidden object-cover w-10 h-10 mx-4 rounded-full sm:block">
-                                        <h1 class="font-bold text-gray-700 hover:underline">Steve Matt</h1>
+                                        <h1 class="font-bold text-gray-700 hover:underline">Jane Doe</h1>
                                     </a></div>
+                                     <!-- Like and Comment Section -->
+                                     <div class="flex items-center justify-between text-gray-500">
+                                        <div class="flex items-center space-x-2">
+                                            <button class="flex justify-center items-center gap-2 px-2 hover:bg-gray-50 rounded-full p-1">
+                                                <svg class="w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                                    <path d="M12 21.35l-1.45-1.32C6.11 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-4.11 6.86-8.55 11.54L12 21.35z" />
+                                                </svg>
+                                                <span>42 Likes</span>
+                                            </button>
+                                        </div>
+                                        <button class="flex justify-center items-center gap-2 px-2 hover:bg-gray-50 rounded-full p-1">
+                                            <svg width="22px" height="22px" viewBox="0 0 24 24" class="w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg">
+                                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                                <g id="SVGRepo_iconCarrier">
+                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22ZM8 13.25C7.58579 13.25 7.25 13.5858 7.25 14C7.25 14.4142 7.58579 14.75 8 14.75H13.5C13.9142 14.75 14.25 14.4142 14.25 14C14.25 13.5858 13.9142 13.25 13.5 13.25H8ZM7.25 10.5C7.25 10.0858 7.58579 9.75 8 9.75H16C16.4142 9.75 16.75 10.0858 16.75 10.5C16.75 10.9142 16.4142 11.25 16 11.25H8C7.58579 11.25 7.25 10.9142 7.25 10.5Z"></path>
+                                                </g>
+                                            </svg>
+                                            <span>3 Comment</span>
+                                        </button>
+                                    </div>
                             </div>
                         </div>
                     </div>
                     <div class="mt-6">
                         <div class="max-w-4xl px-10 py-6 mx-auto bg-white rounded-lg shadow-md">
-                            <div class="flex items-center justify-between"><span class="font-light text-gray-600">Mar 10,
-                                    2018</span><a href="#"
-                                    class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">Testing</a>
+                            <div class="flex items-center justify-between"><span class="font-light text-gray-600">mar 4,
+                                    2019</span><a href="#"
+                                    class="px-2 py-1 font-bold text-gray-100 bg-gray-600 rounded hover:bg-gray-500">Design</a>
                             </div>
-                            <div class="mt-2"><a href="#" class="text-2xl font-bold text-gray-700 hover:underline">TDD
-                                    Frist</a>
+                            <div class="mt-2"><a href="#"
+                                    class="text-2xl font-bold text-gray-700 hover:underline"><img src="https://placekitten.com/400/300" alt="Post Image" class="w-full h-48 object-cover rounded-md"></a>
                                 <p class="mt-2 text-gray-600">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                                     Tempora expedita dicta totam aspernatur doloremque. Excepturi iste iusto eos enim
                                     reprehenderit nisi, accusamus delectus nihil quis facere in modi ratione libero!</p>
                             </div>
-                            <div class="flex items-center justify-between mt-4"><a href="#"
-                                    class="text-blue-500 hover:underline">Read more</a>
+                            <div class="flex items-center justify-between mt-4">
                                 <div><a href="#" class="flex items-center"><img
-                                            src="https://images.unsplash.com/photo-1502980426475-b83966705988?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=373&amp;q=80"
+                                            src="https://images.unsplash.com/photo-1464863979621-258859e62245?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=333&amp;q=80"
                                             alt="avatar" class="hidden object-cover w-10 h-10 mx-4 rounded-full sm:block">
-                                        <h1 class="font-bold text-gray-700 hover:underline">Khatab Wedaa</h1>
+                                        <h1 class="font-bold text-gray-700 hover:underline">Jane Doe</h1>
                                     </a></div>
+                                     <!-- Like and Comment Section -->
+                                     <div class="flex items-center justify-between text-gray-500">
+                                        <div class="flex items-center space-x-2">
+                                            <button class="flex justify-center items-center gap-2 px-2 hover:bg-gray-50 rounded-full p-1">
+                                                <svg class="w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                                                    <path d="M12 21.35l-1.45-1.32C6.11 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-4.11 6.86-8.55 11.54L12 21.35z" />
+                                                </svg>
+                                                <span>42 Likes</span>
+                                            </button>
+                                        </div>
+                                        <button class="flex justify-center items-center gap-2 px-2 hover:bg-gray-50 rounded-full p-1">
+                                            <svg width="22px" height="22px" viewBox="0 0 24 24" class="w-5 h-5 fill-current" xmlns="http://www.w3.org/2000/svg">
+                                                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                                                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                                                <g id="SVGRepo_iconCarrier">
+                                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M12 22C17.5228 22 22 17.5228 22 12C22 6.47715 17.5228 2 12 2C6.47715 2 2 6.47715 2 12C2 13.5997 2.37562 15.1116 3.04346 16.4525C3.22094 16.8088 3.28001 17.2161 3.17712 17.6006L2.58151 19.8267C2.32295 20.793 3.20701 21.677 4.17335 21.4185L6.39939 20.8229C6.78393 20.72 7.19121 20.7791 7.54753 20.9565C8.88837 21.6244 10.4003 22 12 22ZM8 13.25C7.58579 13.25 7.25 13.5858 7.25 14C7.25 14.4142 7.58579 14.75 8 14.75H13.5C13.9142 14.75 14.25 14.4142 14.25 14C14.25 13.5858 13.9142 13.25 13.5 13.25H8ZM7.25 10.5C7.25 10.0858 7.58579 9.75 8 9.75H16C16.4142 9.75 16.75 10.0858 16.75 10.5C16.75 10.9142 16.4142 11.25 16 11.25H8C7.58579 11.25 7.25 10.9142 7.25 10.5Z"></path>
+                                                </g>
+                                            </svg>
+                                            <span>3 Comment</span>
+                                        </button>
+                                    </div>
                             </div>
                         </div>
                     </div>
